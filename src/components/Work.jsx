21004e1198a53ffa976bc1481131a0b60b2b3cbf
Mohit Bhatt <bhattmohit869@gmail.com>
2023-08-07 14:27:40 +0530

    Second Commit

---
+++ src/components/Work.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { data } from "../data/data.js";
+
+const Work = () => {
+  const project = data;
+
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#802774]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my recent work</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-10">
+          {project.map((item, index) => (
+            <div
+              key={item.id}
+              style={{ backgroundImage: `url(${item.image})`}}
+              className="bg-contain shadow-lg  shadow-[#020203] group container rounded-md
+              flex justify-center text-center items-center mx-auto content-div  hover:scale-125 duration-500"
+            >
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font bold text-white tracking-wider">
+                  {item.name}
+                </span>
+                <div className="pt-8 text-center ">
+                  <a href={item.github}>
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Code
+                    </button>
+                  </a>
+
+                  <a href={item.live}>
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Live
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+import MealOpedia from '../assets/projects/MealOpedia.png';
+import faceRecognition from '../assets/projects/face-recognition-attendance-system.jpg';
+import fakeNews from '../assets/projects/fake-news.jpg';
+
+export const data=[
+    {
+        id:1,
+        name:"MealOpedia",
+        image: MealOpedia,
+        github:"https://github.com/MohitBhatt07/MealOpedia",
+        live:"https://github.com/MohitBhatt07/MealOpedia",
+    },
+    {
+        id:2,
+        name:"Fake News Detection",
+        image:fakeNews,
+        github:"https://github.com/MohitBhatt07/FAKE-NEWS-DETECTOR",
+        live:"https://github.com/MohitBhatt07/FAKE-NEWS-DETECTOR",
+    },
+    {
+        id:3,
+        name:"Face recognition based attendance system ",
+        image:faceRecognition,
+        github:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
+        live:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
+    },
+    {
+        id:4,
+        name:"fsaf",
+        image:faceRecognition,
+        github:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
+        live:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
+    },
+
+
+]
