b063bb85e7b303f13502ed3dfb0294827aec1cf8
Mohit Bhatt <bhattmohit869@gmail.com>
2023-09-02 16:16:56 +0530

    deployment changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Work from "./components/Work";
 import Skills from "./components/Skills";
+// import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className={"main-class"}>
       <Navbar />
       <Home/>
       <About />
