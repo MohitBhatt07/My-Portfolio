9797a13992866639ad9ba5e735ade6f1d850575d
Mohit Bhatt <bhattmohit869@gmail.com>
2023-09-13 09:21:15 +0530

    Project page updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,29 +1,31 @@
 import MealOpedia from '../assets/projects/MealOpedia.png';
 import faceRecognition from '../assets/projects/face-recognition-attendance-system.jpg';
 import fakeNews from '../assets/projects/fake-news.jpg';
+import ShopIt from '../assets/projects/shopitImage.jpg' 
 
 export const data=[
     {
         id:1,
+        name:"ShopIt ecommerce Website",
+        image:ShopIt,
+        github:"https://github.com/MohitBhatt07/ShopIt",
+        live:"https://64ff4521866787253517baff--guileless-kheer-4a94a7.netlify.app/",
+    },
+    {
+        id:2,
         name:"MealOpedia",
         image: MealOpedia,
         github:"https://github.com/MohitBhatt07/MealOpedia",
         live:"https://github.com/MohitBhatt07/MealOpedia",
     },
     {
-        id:2,
+        id:3,
         name:"Fake News Detection",
         image:fakeNews,
         github:"https://github.com/MohitBhatt07/FAKE-NEWS-DETECTOR",
         live:"https://github.com/MohitBhatt07/FAKE-NEWS-DETECTOR",
     },
-    {
-        id:3,
-        name:"Face recognition based attendance system ",
-        image:faceRecognition,
-        github:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
-        live:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
-    },
+    
     {
         id:4,
         name:"fsaf",
