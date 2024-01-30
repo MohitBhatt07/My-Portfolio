a630663a891589e262df3aa19d5fe1f7e6b3634e
Mohit Bhatt <bhattmohit869@gmail.com>
2024-01-30 21:01:11 +0530

    project section updated

--- src/data/data.js
+++ src/data/data.js
@@ -2,24 +2,31 @@ import MealOpedia from '../assets/projects/MealOpedia.png';
 import faceRecognition from '../assets/projects/face-recognition-attendance-system.jpg';
 import fakeNews from '../assets/projects/fake-news.jpg';
 import ShopIt from '../assets/projects/shopitImage.jpg' 
-
+import LetsTalk from '../assets/projects/letstalk.jpg';
 export const data=[
     {
         id:1,
+        name:"Lets Talk RealTime Chat App",
+        image: LetsTalk,
+        github:"https://github.com/MohitBhatt07/Lets-Talk",
+        live:"https://lets-talk-34ie.vercel.app//",
+    },
+    {
+        id:2,
         name:"ShopIt ecommerce Website",
         image:ShopIt,
         github:"https://github.com/MohitBhatt07/ShopIt",
         live:"https://shop-it-app.netlify.app/",
     },
     {
-        id:2,
+        id:3,
         name:"MealOpedia",
         image: MealOpedia,
         github:"https://github.com/MohitBhatt07/MealOpedia",
         live:"https://github.com/MohitBhatt07/MealOpedia",
     },
     {
-        id:3,
+        id:4,
         name:"Fake News Detection",
         image:fakeNews,
         github:"https://github.com/MohitBhatt07/FAKE-NEWS-DETECTOR",
@@ -27,12 +34,12 @@ export const data=[
     },
     
     {
-        id:4,
+        id:5,
         name:"Face Recognition Project",
         image:faceRecognition,
         github:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
         live:"https://github.com/MohitBhatt07/Face-Recognition-based-attendance-system",
     },
-
+    
 
 ]
